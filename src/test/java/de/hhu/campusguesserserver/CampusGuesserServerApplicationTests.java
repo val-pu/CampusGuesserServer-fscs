b06e482f6d2d3d308f2bea1ac92fec97c0c8fd08
valentin <val.pukhov@gmail.com>
2024-07-25 22:47:18 +0200

    Init

---
+++ src/test/java/de/hhu/campusguesserserver/CampusGuesserServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hhu.campusguesserserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusGuesserServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
