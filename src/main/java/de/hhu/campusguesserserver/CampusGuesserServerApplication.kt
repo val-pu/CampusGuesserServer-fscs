22da70fa052560a37e075de431254f7aa0950c71
valentin <val.pukhov@gmail.com>
2024-07-27 19:07:49 +0200

    Java zu Kotlin geändert

---
+++ src/main/java/de/hhu/campusguesserserver/CampusGuesserServerApplication.kt
@@ -0,0 +1,12 @@
+package de.hhu.campusguesserserver
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(CampusGuesserServerApplication::class.java, *args)
+}
+
+
+@SpringBootApplication
+open class CampusGuesserServerApplication { }
